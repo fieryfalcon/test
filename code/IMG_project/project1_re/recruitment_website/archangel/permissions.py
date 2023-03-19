80e5804fc67d13a20dcc270bf42b3fda39ef3743
fieryfalcon <rsudarsan369@gmail.com>
2023-03-19 16:23:22 +0530

    initial commit

---
+++ code/IMG_project/project1_re/recruitment_website/archangel/permissions.py
@@ -0,0 +1,10 @@
+# from rest_framework.permissions import BasePermission
+
+# class is_3y_or_4y(BasePermission):
+#     message="****"
+
+#     def has_object_permission(self, request, view,obj):
+#         if obj.year_in_season==2:
+#             return False
+        
+#         return True
